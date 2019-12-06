ca955b4edb362b59c1a0de505479a4cf26c9c38c
A <a@a.a>
2019-12-06 19:26:43 +0200

    string test

--- src/main/java/JsoupTest.java
+++ src/main/java/JsoupTest.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 public class JsoupTest {
     public static void main(String[] args) throws IOException {
         String url = "https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D0%B7%D1%80%D0%B5%D1%88%D0%B5%D0%BD%D0%B8%D0%B5_(%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80%D0%BD%D0%B0%D1%8F_%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%BA%D0%B0)";
-
-
         Document doc = Jsoup.connect(url)
                 .userAgent("Mozilla")
                 .get();
@@ -26,14 +24,12 @@ public class JsoupTest {
                         System.out.println();
                         System.out.println(words.get(j).text());
                     } else  {
-                        System.out.printf("%-32s", words.get(j).text());
+                        System.out.printf("%-24s", words.get(j).text());
                     }
                 }
                 System.out.println();
-
-
             }
-            System.out.println("-------------------------------------------------------------------------------");
+            System.out.println("-----------------------------------------------------------------------");
         }
     }
 }
