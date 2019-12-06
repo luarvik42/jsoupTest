c7f7b8981299dddfde0aa48e56098554f94569e0
luarvik42 <lluarvik42@gmail.com>
2019-12-06 19:21:52 +0200

    test 1

--- src/main/java/JsoupTest.java
+++ src/main/java/JsoupTest.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 public class JsoupTest {
     public static void main(String[] args) throws IOException {
         String url = "https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D0%B7%D1%80%D0%B5%D1%88%D0%B5%D0%BD%D0%B8%D0%B5_(%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80%D0%BD%D0%B0%D1%8F_%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%BA%D0%B0)";
+
+
         Document doc = Jsoup.connect(url)
                 .userAgent("Mozilla")
                 .get();
@@ -28,6 +30,8 @@ public class JsoupTest {
                     }
                 }
                 System.out.println();
+
+
             }
             System.out.println("-------------------------------------------------------------------------------");
         }
